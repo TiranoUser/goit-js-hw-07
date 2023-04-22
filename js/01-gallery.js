75a5b2fbdd2b6148c9f808485b0e6ae79c8f86cd
TiranoUser <v130561@gmail.com>
2023-04-22 16:29:22 +0200

    01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkUp(galleryItems));
+
+galleryEl.addEventListener('click', modalShow);
+
+function modalShow(event) {
+  event.preventDefault();
+  basicLightbox
+    .create(`<img width="1280" src="${event.target.dataset.source}">`)
+    .show();
+}
