9df40bcdd0bcc96e9ca45a4a9ff06457d5b9757c
TiranoUser <v130561@gmail.com>
2023-04-22 17:55:40 +0200

    work complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link lightbox" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkUp(galleryItems));
+
+galleryEl.addEventListener('click', modalShow);
+
+function modalShow(event) {
+  event.preventDefault();
+
+  new SimpleLightbox('.gallery a', {
+    /* options */
+    opacity: 0.65,
+    navText: ['◀', '▶'],
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
